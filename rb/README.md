dca5caa8895c876a43623de394675a50f0281ac6
hamstersky <choomik21@gmail.com>
2016-10-08 18:01:41 +0200

    Update main README and untrack some files

--- rb/README.md
+++
@@ -1,5 +0,0 @@
-<h1>Ruby Project</h1>
-Learning Ruby as part of
-<a href="www.theodinproject.com">The Odin Project</a>
-by using test driven development exercises from 
-<a href="http://testfirst.org/learn_ruby">testfirst.org</a>
--- ruby_euler/multiples_of_3_and_5.rb
+++
@@ -1,8 +0,0 @@
-def multiples(max)
-	#three = (1..max).select { |a| a % 3 == 0 }
-	#five = (1..max).select { |a| a % 5 == 0 }
-	multiples = (1...max).select { |a| a % 3 == 0 || a % 5 == 0}
-	multiples.inject(:+)
-end
-
-p multiples(1000)
