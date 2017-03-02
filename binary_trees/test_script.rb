6a6073974211a200c710c13924893e0a22caddf0
Hamstersky <choomik21@gmail.com>
2017-03-02 19:52:25 +0100

    edit test script

--- binary_trees/test_script.rb
+++ binary_trees/test_script.rb
@@ -2,10 +2,10 @@ require_relative 'binary_trees'
 
 bst = BinarySearchTree.new
 bst.build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-# puts bst.root.value
-# puts bst.root.right.value
-# puts bst.root.right.right.value
-# puts bst.root.right.left.value
 
-puts bst.bfs(11)
-puts bst.dfs(11)
+puts bst.bfs(23)
+puts bst.dfs(23)
+puts bst.dfs_rec(23)
+p bst.bfs(11)
+p bst.dfs(11)
+p bst.dfs_rec(11)
